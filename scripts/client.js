dbb3771b561ea83bc1daa28e3f05bd7de3da940c
Joshua McCollum <xmtuner629@yahoo.com>
2021-09-05 01:04:06 -0500

    added lots more to do

--- scripts/client.js
+++ scripts/client.js
@@ -1,6 +1,21 @@
 $(document).ready(onReady);
 
-function onReady(){
-   
 
+function onReady(){
+   $("#addEmployeeButton").on('click', addEmployee)
+   $("#table").on('click','.deleteButton', deleteEmployee);
+    
 } // end onReady
+let totalSalary = 0;
+function addEmployee (){
+    console.log('addEmployee function');
+    let el = $('table tbody')
+    el.append('<tr div class = "person"> <td>'+ $('#fName').val()+' </td> <td> '+ $('#lName').val()+ ' </td> <td>'+ $('#idNum').val()+'</td> <td>'+ $('#jobTitle').val()+' </td> <td>' + parseInt($('#salary').val(), 10) + '</td><td><button class = "deleteButton">Delete</button></td></tr></div>')
+    totalSalary += parseInt($('#salary').val(), 10);
+}// end add employee
+
+function deleteEmployee(){
+console.log("deleteEmployee function");
+
+$(this).parent().parent().remove();
+}
