acb267fa0fe58792dd1b78296875bce51653feeb
Joshua McCollum <xmtuner629@yahoo.com>
2021-09-07 21:53:42 -0500

    updated totals

--- scripts/client.js
+++ scripts/client.js
@@ -8,17 +8,19 @@ function onReady(){
 
 } // end onReady
 let totalSalary = 0;
-let counter = 0
+
 
 // adding the employees
 function addEmployee (){
     
-    counter++
+    
 
     console.log('addEmployee function');
     let el = $('table tbody');
-    
-    el.append('<tr div id = "person"><td>'+ $('#fName').val()+'</td> <td> '+ $('#lName').val()+ ' </td> <td>'+ $('#idNum').val()+'</td> <td>'+ $('#jobTitle').val()+' </td> <td class = "employeeSalary">' + parseInt($('#salary').val(), 10) + '</td><td><button class = "deleteButton">Delete</button></td></tr></div>')
+    //not pretty but adds a row with data 
+    el.append('<tr div id = "person"><td>'+ $('#fName').val()+'</td> <td> '+ $('#lName').val()+
+     ' </td> <td>'+ $('#idNum').val()+'</td> <td>'+ $('#jobTitle').val()+' </td> <td class = "employeeSalary">' +
+      parseInt($('#salary').val(), 10) + '</td><td><button class = "deleteButton">Delete</button></td></tr></div>')
     totalSalary += parseInt($('#salary').val(), 10);
     
     // return fields to empty
@@ -50,13 +52,18 @@ updateTotal()
 
 
 function updateTotal (){
-    let el = $("#totalSalary");
-    el.empty();
-    el.append('Total Salary = '+ totalSalary)
-    if (totalSalary > 20000){
+    let monthlyTotal = parseFloat(totalSalary / 12).toFixed(2);
+    $("#monthlySalary").empty();
+    $("#monthlySalary").append('Monthly Total = $'+ monthlyTotal)
+    $("#totalSalary").empty();
+    $("#totalSalary").append('Total Salary = $'+ totalSalary)
+
+    if (monthlyTotal > 20000){
+        $('#monthlySalary').addClass('red')
         $('#totalSalary').addClass('red')
     }
     else{
+        $('#monthlySalary').removeClass('red')
         $('#totalSalary').removeClass('red')
     }
 }
